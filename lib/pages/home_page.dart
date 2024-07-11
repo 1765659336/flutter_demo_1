553f2085620d7a31bc062e64a2213f63853671d3
LiuJie <1765659336@qq.com>
2024-07-11 11:01:36 +0800

    feat: 日志与路由跳转

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_1/pages/web_view_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+import 'package:logger/logger.dart';
+
+final Logger logger = Logger();
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -55,33 +59,40 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _listViewItem(index) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15), // 设置内边距 左上右下
-      child: const Column(children: [
-        Row(
-          children: [
-            Text("#001"),
-            SizedBox(width: 8.0),
-            Text("[审核事务板块]事务审核页面卡片中字段对齐问题"),
-          ],
-        ),
-        Row(
-          children: [
-            Text("功能"),
-            Text("高"),
-            Text("未开发"),
-            Text("潘玥"),
-            Text("刘杰"),
-          ],
-        ),
-        Row(
-          children: [
-            Text("20240711版本"),
-            Text("2024-07-09"),
-            Text("未发布"),
-          ],
-        ),
-      ]),
-    );
+    return InkWell(
+        onTap: () {
+          logger.d('点击了第 $index 项');
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return WebViewPage(title: '第${index + 1}条数据标题');
+          }));
+        },
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(0, 15, 0, 15), // 设置内边距 左上右下
+          child: const Column(children: [
+            Row(
+              children: [
+                Text("#001"),
+                SizedBox(width: 8.0),
+                Text("[审核事务板块]事务审核页面卡片中字段对齐问题"),
+              ],
+            ),
+            Row(
+              children: [
+                Text("功能"),
+                Text("高"),
+                Text("未开发"),
+                Text("潘玥"),
+                Text("刘杰"),
+              ],
+            ),
+            Row(
+              children: [
+                Text("20240711版本"),
+                Text("2024-07-09"),
+                Text("未发布"),
+              ],
+            ),
+          ]),
+        ));
   }
 }
